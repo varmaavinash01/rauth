8bcd8e9ed0e660b29952330ca0c581c23e9540c1
Avi-ec2-kyp <ubuntu@ip-10-98-171-12.ec2.internal>
2013-05-21 12:49:47 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rauth::Application.config.secret_token = 'b95b3eb89a78eb39b926d4aa74d6d9b9c38b0f511b36aa18432ea0c03c901c08c7ff71126a932d22aa5a0ecd04353d3095e89d1014678615c5b8e28b60b0e94e'
